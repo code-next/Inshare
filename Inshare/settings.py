c4fa099bbaaf695f313a3ad15d634050c90a0b6d
Nijinsha <mail.nijinsha@gmail.com>
2018-01-15 18:40:36 +0530

    adds redis,tasks.py

--- Inshare/settings.py
+++ Inshare/settings.py
@@ -176,3 +176,10 @@ JWT_AUTH = {
     'JWT_AUTH_COOKIE': None,
 
 }
+
+# REDIS related settings
+REDIS_HOST = 'localhost'
+REDIS_PORT = '6379'
+BROKER_URL = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
+BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 3600}
+CELERY_RESULT_BACKEND = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
