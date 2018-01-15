1a457819c77d016b055ef4839e29a680b4f5bd71
Prakhil-tp <prakhil.tp@gmail.com>
2018-01-16 02:42:58 +0530

    changes for the dev

--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,6 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Person(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_pic = models.ImageField(null=True)
+    profile_pic = models.FileField(null=True)
     def __str__(self):
         return str(self.user.first_name)
