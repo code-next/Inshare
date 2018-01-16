7e4f27a4c6fa988505028d2a44a2b170b720dc63
Nijinsha <mail.nijinsha@gmail.com>
2018-01-16 16:16:20 +0530

    adds gallery models

--- gallery/models.py
+++ gallery/models.py
@@ -7,3 +7,6 @@ class Photo(models.Model):
     owner = models.ForeignKey(User,on_delete=models.CASCADE)
     image = models.ImageField()
     date = models.DateField()
+    
+    def __str__(self):
+        return  self.owner + "-" + self.date
