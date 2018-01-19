e0f950b4ca8fd9955f3c4677a8932f25dff303f9
Nijinsha <mail.nijinsha@gmail.com>
2018-01-19 19:20:06 +0530

    adds serializer

--- gallery/models.py
+++ gallery/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
+from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFill
+
 # Create your models here.
 
 
 class Photo(models.Model):
-    owner = models.ForeignKey(User,on_delete=models.CASCADE)
-    image = models.ImageField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='media')
+    image_thumb = ImageSpecField(source='image',
+                                 processors=[ResizeToFill(200, 200)],
+                                 format='JPEG',
+                                 options={'quality': 60})
     date = models.DateField()
+
--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .serializers import PhotoSerializer
 # Create your views here.
+
+
