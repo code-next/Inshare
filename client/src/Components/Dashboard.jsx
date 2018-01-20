8f28b8f49440eb5092e868349cf463692942617c
Prakhil-tp <prakhil.tp@gmail.com>
2018-01-21 02:51:31 +0530

    basic panels and ui stuff

--- client/src/Components/Dashboard.jsx
+++ client/src/Components/Dashboard.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
+import { Drawer, Grid } from 'material-ui';
+import profileDp from '../images/dp.png';
+import './Dashboard.css';
 
 class Dashboard extends Component {
   constructor(props) {
@@ -7,6 +10,10 @@ class Dashboard extends Component {
     this.state = {
       isLoggedIn: false,
       token: '',
+      dpSrc: '',
+      photoCount: 286,
+      firstName: 'Tony',
+
     };
   }
   componentWillMount() {
@@ -17,8 +24,38 @@ class Dashboard extends Component {
   }
   render() {
     return (
-      (this.state.isLoggedIn) ? <h1>this is your Dashboard</h1> :
-      <Redirect to="/" />
+      (this.state.isLoggedIn) ?
+        <div>
+          <Drawer type="permanent">
+            <div className="gal-drawer">
+              <Grid container className="gal-drawer-grid" >
+                <Grid item lg={12} xs={12} className="gal-whitespace" />
+                <Grid item lg={3} />
+                <Grid item lg={6}>
+                  <div
+                    className="gal-dp-image-container"
+                    style={{
+                      background: `url(${(this.state.dpSrc === '') ? profileDp : this.state.dpSrc}) repeat center top`,
+                      backgroundSize: '100px 100px',
+                    }}
+                  />
+                </Grid>
+                <Grid item lg={3} />
+                <Grid item lg={2} />
+                <Grid item lg={8}>
+                  <div className="gal-name">
+                    <span className="first">{this.state.firstName}</span><br />
+                    <span className="photo-count">{this.state.photoCount} files</span>
+                  </div>
+                </Grid>
+                <Grid item lg={2} />
+              </Grid>
+            </div>
+          </Drawer>
+          {/* actual content goes here */}
+        </div>
+        :
+        <Redirect to="/" />
     );
   }
 }
--- client/src/Components/SignUp.jsx
+++ client/src/Components/SignUp.jsx
@@ -27,10 +27,30 @@ class SignUp extends Component {
     this.handleModelClose = this.handleModelClose.bind(this);
     this.signUpPostRequest = this.signUpPostRequest.bind(this);
   }
-  handleFirstName(e) { this.setState({ first_name: e.target.value }); }// setting firstname value
-  handleEmail(e) { this.setState({ email: e.target.value }); }// setting value of email
-  handlePassword(e) { this.setState({ password: e.target.value }); }// setting value of password
-  handleNext() { this.setState({ dp: true }); }// directing the view.(handling Next button)
+  // runs when user enter the name
+  handleFirstName(e) {
+    this.setState({
+      first_name: e.target.value,
+    });
+  }
+  // runs when user enter email
+  handleEmail(e) {
+    this.setState({
+      email: e.target.value,
+    });
+  }
+  // runs when user enter password
+  handlePassword(e) {
+    this.setState({
+      password: e.target.value,
+    });
+  }
+  // runs when user click the next button
+  handleNext() {
+    this.setState({
+      dp: true,
+    });
+  }
   // retrieve the path from the image-upload. And open the Modal
   handleUploadImage(e) {
     e.preventDefault();
@@ -44,14 +64,14 @@ class SignUp extends Component {
     };
     reader.readAsDataURL(file);
   }
-  // changing the crop image path
+  // changing the img path to crop image path
   handleModelClose() {
     this.setState({
       openModel: false,
       imgSrc: this.cropElement.crop(),
     });
   }
-  // signUp post request
+  // signUp post request - runs whenever the SIGN UP button clicks.
   signUpPostRequest() {
     fetch('http://localhost:8000/auth/register/', {
       method: 'POST',
@@ -89,7 +109,12 @@ class SignUp extends Component {
               }}
             >
               <div className="dp-hover-cover">
-                <a onClick={() => { this.inpuElement.click(); }}>
+                <a
+                  role="button"
+                  onClick={() => { this.inpuElement.click(); }}
+                  tabIndex="0"
+                  onKeyPress={() => { this.inpuElement.click(); }}
+                >
                   <img src={uploadIcon} alt="uplad icon" className="upload-icon" />
                   <input
                     type="file"
