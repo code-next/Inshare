f6d7b28c3dcad2d91e2f2af6610f4391bac71161
Nijinsha <mail.nijinsha@gmail.com>
2018-02-06 23:04:20 +0530

    removes the urls for profile pic

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from rest_framework_jwt.views import obtain_jwt_token
-from .views import UserCreateView,ProfilePictureView
+from .views import UserCreateView
 
 
 urlpatterns = [
     path('register/', UserCreateView.as_view(), name="registration"),
     path('login/', obtain_jwt_token, name="login"),
-    path('profile-pic/',ProfilePictureView.as_view(),name="profile-pic-upload")
+    
 ]
