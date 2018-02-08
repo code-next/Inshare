a81b8490a401c494e2bd56978d7ce5519d59a82b
Nijinsha <mail.nijinsha@gmail.com>
2018-02-08 08:38:11 +0530

    removes api code which is unnecessary

--- api/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        fields = ('pk', 'email', 'password', 'first_name', 'last_name')
-        read_only_fields = ('pk',)
